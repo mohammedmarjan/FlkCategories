9aa6967ae441ceb94eaee40d1e5c3b993af12211
mohammedmarjan <mohammedmarjan786@gmail.com>
2022-08-15 09:11:27 +0530

    added find method to increase perfomance

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 "use strict";
-const AWS = require("aws-sdk"); // eslint-disable-line import/no-extraneous-dependencies
+const AWS = require("aws-sdk"); 
+//import data from local js file
 const data = require("./data.js");
 
 module.exports.get = (event, context, callback) => {
   // fetch requested category from path Params
   const { category } = event.pathParameters;
+  const result = [];
   // fetch category data
-  const categoryData = data.filter((item) => item.category_name === category);
+  const categoryData = data.find((item) => item.category_name === category);
+  // add data to the result array
+  categoryData && result.push(categoryData);
   // create a response
   const response = {
     statusCode: 200,
-    body: JSON.stringify(categoryData),
+    body: JSON.stringify(result),
   };
   callback(null, response);
 };
 
 module.exports.list = (event, context, callback) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(data),
-      };
-      callback(null, response);
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+  callback(null, response);
 };
